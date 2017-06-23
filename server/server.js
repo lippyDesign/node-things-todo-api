e7b22b4f9d7d4cbed382c8fa1955bd0139977cd3
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-06-22 18:39:28 -0700

    test post todos route

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db/mongoose');
 
-const { Todo } = require('./models/Todo');
-const { User } = require('./models/User');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 
@@ -22,3 +22,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+module.exports = { app };
