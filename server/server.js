3026caca7d1c89856a72f69a99fe5ea14c5fbb8c
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-06-22 20:36:58 -0700

    added get todos route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//POST Todo
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
@@ -17,6 +18,15 @@ app.post('/todos', (req, res) => {
   todo.save()
     .then(doc => res.send(doc))
     .catch(e => res.status(400).send(e));
+});
+
+//Get Todo
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos });
+    })
+    .catch(error => res.status(400).send(error));
 });
 
 app.listen(3000, () => {
