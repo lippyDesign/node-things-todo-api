835bf1f5d0b8ec7c471de549d5aab2d773dde065
Volodymyr Lipunov <volo_lipu@yahoo.com>
2017-06-23 21:41:29 -0700

    added delete tests

--- server/server.js
+++ server/server.js
@@ -45,13 +45,14 @@ app.get('/todos/:id', (req, res) => {
     .catch(() => res.status(400).send());
 })
 
+// DELETE todos/:id
 app.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
   if (!ObjectID.isValid(id)) return res.status(404).send();
   Todo.findByIdAndRemove(id)
     .then(todo => {
       if (!todo) return res.status(404).send();
-      res.send(todo);
+      res.send({ todo });
     })
     .catch(() => res.status(400).send());
 });
